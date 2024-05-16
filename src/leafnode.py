f66c6f618e3f93a9da493ce4953ce1f3c2dcd47f
Jack Leatherland <jack.leatherland@qbe.com>
2024-05-16 16:41:52 +0100

    ch 2.5

--- src/leafnode.py
+++ src/leafnode.py
@@ -1,11 +1,14 @@
 from htmlnode import HTMLNode 
 
 class LeafNode(HTMLNode):
-    def __init__(self, tag, value, props):
-        super().__init__(tag, value, props)
+    def __init__(self, tag, value, props = None):
+        super().__init__(tag = tag, value = value, props = props)
 
     def to_html(self):
-        if (!self.value):
+        tag_prop = self.tag
+        if (not self.value):
             raise ValueError('A leaf requires a value')
-        return self.props_to_html()
+        if (self.props):
+            tag_prop += f" {self.props_to_html()}"
+        return f"<{tag_prop}>{self.value}</{self.tag}>"
 
---
+++ src/test_leafnode.py
@@ -0,0 +1,21 @@
+import unittest
+
+from leafnode import LeafNode
+
+class LeafTestNode(unittest.TestCase):
+    def test_to_html(self):
+        node = LeafNode("p", "This is a paragraph of text.")
+        expected_result = "<p>This is a paragraph of text.</p>"
+        self.assertEqual(node.to_html(), expected_result)
+
+    def test_to_html_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        expected_result = '<a href="https://www.google.com">Click me!</a>'
+        self.assertEqual(node.to_html(), expected_result)
+    
+    def missing_value(self):
+        node = LeafNode("p")
+        expected_result = ValueError('A leaf requires a value')
+        self.assertEqual(node.to_html(), expected_result)
+if __name__ == "__main__":
+    unittest.main()
