1b6ebfce35df66cdca93233678b7f904510a3711
Jack Leatherland <jack.leatherland@qbe.com>
2024-05-24 16:16:20 +0100

    extract methods for link and image

---
+++ src/test_extract_methods.py
@@ -0,0 +1,28 @@
+import unittest
+
+from extract_methods import (
+    extract_markdown_images,
+    extract_markdown_links
+)
+
+class TestExtractMethods(unittest.TestCase):
+    def test_image_extract(self):
+        text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
+        expected = [("image", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png"), ("another", "https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png")]
+        result = extract_markdown_images(text)
+        self.assertListEqual(expected, result)
+
+    def test_link_extract(self):
+        text = "This is text with a [link](https://www.example.com) and [another](https://www.example.com/another)"
+        expected = [("link", "https://www.example.com"), ("another", "https://www.example.com/another")]
+        result = extract_markdown_links(text)
+        self.assertListEqual(expected, result)
+
+    def test_link_is_not_an_image(self):
+        text = "This is text with an ![image](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/zjjcJKZ.png) and ![another](https://storage.googleapis.com/qvault-webapp-dynamic-assets/course_assets/dfsdkjfd.png)"
+        expected = []
+        result = extract_markdown_links(text)
+        self.assertListEqual(expected, result)
+        
+if __name__ == "__main__":
+    unittest.main()
